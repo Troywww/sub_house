a2642fdac3150d2501a155af1c3881c17713808b
Troywww <166276911+Troywww@users.noreply.github.com>
2025-12-16 22:20:04 +0800

    Update clash.js

--- subscription/clash.js
+++ subscription/clash.js
@@ -355,7 +355,7 @@ function convertVless(node) {
     if (node.settings.security === 'reality') {
         config.flow = 'xtls-rprx-vision';
         config['reality-opts'] = {
-            'public-key': node.settings.pbk
+            'public-key': node.settings.pbk,
             'short-id': node.settings.sid
         };
         config['client-fingerprint'] = node.settings.fp || 'chrome';
